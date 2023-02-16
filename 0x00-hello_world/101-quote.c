7768226a825c2ecabfae9dc11d55cc5332120c92
HazemZidane <hazemzidane705@gmail.com>
2023-02-17 01:28:54 +0200

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+
+
 /**
  * main - Entery pint
  *
@@ -7,7 +9,6 @@
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
-
