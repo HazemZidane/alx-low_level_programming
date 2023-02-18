2463bfbcb2c49c1fa4afca8cd59eb50be25cf9b1
HazemZidane <hazemzidane705@gmail.com>
2023-02-18 14:16:58 +0200

    0-1 tasks

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,9 +5,6 @@
 /*
  * main - Entery point
  *
- * Prints the last digit of a randomly generated number
- * and whether it is greater than 5, less than 6, or 0.
- *
  * Return: Always 0 (Success)
  */
 
