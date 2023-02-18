8e3dd0ea448077bdc9df81108d04c0e270e472d9
HazemZidane <hazemzidane705@gmail.com>
2023-02-18 15:10:24 +0200

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - Entery point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	fot(ch = 'a'; ch <='z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
