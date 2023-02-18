5eb04cb276c1983947d9fd322893f51e5f6fc149
HazemZidane <hazemzidane705@gmail.com>
2023-02-18 15:49:18 +0200

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
- * main - main block
- * Description: Get a random it with S
+/**
+ * 
+ * main - Entery
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
