e92978ed5bd071d59bc7d4321da9ab1ee0c8222c
HazemZidane <hazemzidane705@gmail.com>
2023-02-21 01:57:58 +0200

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -25,7 +25,7 @@ int main(void)
 					putchar('0' + d);
 					putchar('0' + c);
 
-					if (c + d + e != 9 + 8 +7)
+					if (c + d + e != 9 + 8 + 7)
 					{
 						putchar(',');
 						putchar(' ');
@@ -39,4 +39,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}	
+}
