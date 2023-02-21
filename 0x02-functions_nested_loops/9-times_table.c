060afda46636a113698349ebfe2e6359619051b7
HazemZidane <hazemzidane705@gmail.com>
2023-02-21 22:03:45 +0200

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 
 /**
  *  * times_table - prints the 9 times table, starting with 0
- *   */
+ */
 void times_table(void)
 {
 	int i, j, k;
@@ -33,7 +33,6 @@ void times_table(void)
 				_putchar((k % 10) + '0');
 			}
 		}
-		
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
