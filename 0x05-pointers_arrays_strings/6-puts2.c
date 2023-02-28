13ec43fcdea06a213ec2e46a00d8428b2dae9863
HazemZidane <hazemzidane705@gmail.com>
2023-02-28 22:33:54 +0200

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,20 +5,20 @@
  * * puts2 - function that prints a string, followed by a new line, to stdout
  * * @str: pointer value
  * * Return: Always 0 (Success)
- * */
+ */
 
 void puts2(char *str)
 {
-		int i;
+	int i;
 
-			i = 0;
+	i = 0;
 
-				while (str[i] != '\0')
-						{
-									if (i % 2 == 0)
-													_putchar(str[i]);
-											 i++;
-											 	}
-
-					_putchar('\n');
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+			i++;
+	}
+	
+	_putchar('\n');
 }
