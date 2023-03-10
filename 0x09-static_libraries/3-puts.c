1faba9f28c07e713143e537fcaba07898b72483e
Hazem <hazemzidane705@gmail.com>
2023-03-10 14:12:31 -0800

    task 1 and 2

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _puts - Writes string to stdout
+ * @str: string
  */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
 
+void _puts(char *str)
+{
+        while (*str != '\0')
+        {
+                _putchar(*str++);
+        }
+                _putchar('\n');
+}
